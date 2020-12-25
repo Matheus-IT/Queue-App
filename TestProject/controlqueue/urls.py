03fd4abf596de57fcaed078655c9b7a063539088
Matheus da Costa da Silva <matheuscosta.tech@gmail.com>
2020-12-25 09:58:19 -0300

    Notice for users trying to enter on the queue admin user page

--- TestProject/controlqueue/urls.py
+++ TestProject/controlqueue/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'controlqueue'
 
 urlpatterns = [
-	path('', views.restrict, name='restrict')
+	path('', views.restrict, name='restrict'),
+	path('notice_admin_only/', views.notice_admin_only, name='notice_admin_only')
 ]
--- TestProject/controlqueue/views.py
+++ TestProject/controlqueue/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
+
 def admin_user(user):
 	return user.is_superuser
 
-@user_passes_test(admin_user, login_url='/admin/')
+@user_passes_test(admin_user, login_url='controlqueue:notice_admin_only')
 def restrict(request):
 	return render(request, 'controlqueue/restrict.html')
+
+
+def notice_admin_only(request):
+	return render(request, 'controlqueue/noticeAdminOnly.html')
