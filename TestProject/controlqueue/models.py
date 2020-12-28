ec7188bef968e5dad020c3629ae407fb40cd7771
Matheus da Costa da Silva <matheuscosta.tech@gmail.com>
2020-12-28 09:14:02 -0300

    New Model 'Queue' to manage the number of people awaiting in the barber shop.

--- TestProject/controlqueue/models.py
+++ TestProject/controlqueue/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Queue(models.Model):
+	num_of_people = models.PositiveSmallIntegerField()
+
+	def __str__(self):
+		return f'Number of people {self.num_of_people}'
