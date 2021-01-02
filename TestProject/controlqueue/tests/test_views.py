cf59cab1e4121541e4bfaa91fa95529a2d9e0254
Matheus da Costa da Silva <matheuscosta.tech@gmail.com>
2021-01-02 17:58:33 -0300

    New tests for controlqueue's views

--- TestProject/controlqueue/tests/test_views.py
+++ TestProject/controlqueue/tests/test_views.py
@@ -6,12 +6,41 @@ APP_NAME = 'controlqueue'
 
 class Restrict(TestCase):
 	def setUp(self):
-		self.my_admin = User(username='user_test', is_staff=True, is_superuser=True)
-		self.my_admin.set_password('12345')
+		self.my_admin_data = {
+			'username': 'user_test',
+			'password': '12345'
+		}
+		
+		self.my_admin = User(username=self.my_admin_data['username'], is_staff=True, is_superuser=True)
+		self.my_admin.set_password(self.my_admin_data['password'])
 		self.my_admin.save()
-
+	
 	def test_get_as_anonymous_user(self):
-		self.client.login(username='user_test', password='12345')
 		response = self.client.get(reverse(f'{APP_NAME}:restrict'))
+		self.assertEqual(response.status_code, 302)
+
+	def test_get_as_anonymous_user_following_redirects(self):
+		response = self.client.get(reverse(f'{APP_NAME}:restrict'), follow=True)
+		self.assertEqual(response.status_code, 200)
+
+	def test_get_as_superuser(self):
+		self.client.login(
+			username = self.my_admin_data['username'],
+			password = self.my_admin_data['password']
+		)
+		response = self.client.get(reverse(f'{APP_NAME}:restrict'))
+
+		self.assertEqual(response.status_code, 200)
+	
+	def test_get_notice_admin_only_as_anonymous_user(self):
+		response = self.client.get(reverse(f'{APP_NAME}:notice_admin_only'))
+		self.assertEqual(response.status_code, 200)
+
+	def test_get_notice_admin_only_as_superuser(self):
+		self.client.login(
+			username = self.my_admin_data['username'],
+			password = self.my_admin_data['password']
+		)
+		response = self.client.get(reverse(f'{APP_NAME}:notice_admin_only'))
 
 		self.assertEqual(response.status_code, 200)
