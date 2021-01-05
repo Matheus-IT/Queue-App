d93512998381f3a440e9e267b1503067666b19d0
Matheus da Costa da Silva <matheuscosta.tech@gmail.com>
2021-01-05 10:34:32 -0300

    Init file for package tests in 'testapp' and some changes for 'routing.py' in controlqueue

--- controlqueue/routing.py
+++ controlqueue/routing.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from . import consumers
+from .apps import ControlqueueConfig
 
+app_name = ControlqueueConfig.name
 
 websocket_urlpatterns = [
-	path('ws/queue/', consumers.QueueConsumer.as_asgi()),
-	path('ws/queue/<str:group_name>/', consumers.QueueConsumer.as_asgi()),
+	path('ws/queue/', consumers.QueueConsumer.as_asgi(), name='queue'),
+	path('ws/queue/<str:group_name>/', consumers.QueueConsumer.as_asgi(), name='queue_group'),
 ]
